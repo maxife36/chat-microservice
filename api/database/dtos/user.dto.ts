7a833b8d6deae53f9024966dfca33a61b122bc94
maxife36 <maxife36@gmail.com>
2024-10-28 10:49:36 -0300

    add updateUser Dto

--- api/database/dtos/user.dto.ts
+++ api/database/dtos/user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional, IsString, Length } from "class-validator";
 import { plainToClass } from "class-transformer";
 
 export class UserDto {
@@ -13,10 +13,31 @@ export class UserDto {
 
   @IsNotEmpty()
   @IsString()
-  @Length(6, 50) // Ejemplo de longitud mínima para una contraseña
+  @Length(6, 50) 
   password!: string;
 
   static fromPlain(plain: object): UserDto {
     return plainToClass(UserDto, plain);
   }
 }
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  username?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(6, 50)
+  password?: string;
+
+  static fromPlain(plain: object): UpdateUserDto {
+    return plainToClass(UpdateUserDto, plain);
+  }
+}
+
