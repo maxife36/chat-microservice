f52af4d7e2f64b0bb023b4dcdfbcbd6638eaab15
maxife36 <maxife36@gmail.com>
2024-10-28 00:45:57 -0300

    add gateway database proxy

--- api/gateway/main.ts
+++ api/gateway/main.ts
@@ -6,11 +6,17 @@ dotenv.config()
 
 const PORT = process.env.GATEWAY_CONTAINER_PORT ?? 3000
 const AUTH_PORT = process.env.AUTH_CONTAINER_PORT ?? 3001
+const DATABASE_PORT = process.env.DATABASE_CONTAINER_PORT ?? 3003
 
 const app = express()
 
 app.use("/auth", createProxyMiddleware({
     target:`http://auth:${AUTH_PORT}`,
+    changeOrigin: true
+}))
+
+app.use("/database", createProxyMiddleware({
+    target:`http://database:${DATABASE_PORT}`,
     changeOrigin: true
 }))
 
