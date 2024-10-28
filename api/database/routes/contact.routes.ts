e5f041b8b98f916319c4ccc4a787d932454e96b8
maxife36 <maxife36@gmail.com>
2024-10-28 00:44:12 -0300

    add database routes

---
+++ api/database/routes/contact.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+    getAllContacts,
+    getContactRegister,
+    getUserContacts,
+    createContact,
+    updateContact,
+    deleteContact
+} from "../controllers/contact.controllers";
+
+const router = Router();
+
+router.get("/", getAllContacts);
+router.get("/user/:userId", getUserContacts);
+router.get("/:id", getContactRegister);
+router.post("/", createContact);
+router.put("/:id", updateContact);
+router.delete("/:id", deleteContact);
+
+export default router;
---
+++ api/database/routes/user.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controllers/user.controllers";
+
+const router = Router();
+
+router.get("/", getAllUsers);
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
