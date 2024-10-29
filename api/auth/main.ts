6a7346bcdcb9de9221c4780744435939dc74c640
maxife36 <maxife36@gmail.com>
2024-10-29 10:20:55 -0300

    modify app routes and middlewares

--- api/auth/main.ts
+++ api/auth/main.ts
@@ -1,14 +1,23 @@
 import express, { Request, Response } from "express"
 import dotenv from "dotenv"
+import cookieParser from 'cookie-parser';
+import errorHandler from "./src/handlers/errorHandler"
+import authRoute from "./src/routes/auth.routes"
 dotenv.config()
 
 const PORT = process.env.AUTH_CONTAINER_PORT ?? 3001
 
 const app = express()
 
-app.get("/", (req:Request,res:Response) => {res.send("HOLA MUNDO")})
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", authRoute)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Auth microservice up on port ${PORT}`);
 })
+
